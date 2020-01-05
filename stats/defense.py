835bf09c79ce9c17bda429b24e95d1017fc97fe1
Collin D'Mello <4390162+cldmello@users.noreply.github.com>
2020-01-05 16:57:48 -0500

    Update defense.py

--- stats/defense.py
+++ stats/defense.py
@@ -10,10 +10,10 @@ pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', '
 pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name='PA')
 
 events = events.set_index(['year', 'game_id', 'team', 'event_type'])
-events = events.unstack.fillna(0).reset_index()
-events.columns.droplevel()
+events = events.unstack().fillna(0).reset_index()
+events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
-events = events.remove_axis(None, axis='columns')
+events = events.rename_axis(None, axis='columns')
 
 events_plus_pa = pd.merge(events, pa, how='outer', left_on = ['year', 'game_id', 'team'], right_on = ['year', 'game_id', 'team'])
 defense = pd.merge(events_plus_pa, info)
@@ -21,6 +21,6 @@ defense.loc[:, 'DER'] = 1 - ((defense['H'] + defense['ROE']) / (defense['PA'] - 
 defense.loc[:, 'year'] = pd.to_numeric(defense['year'])
 
 der = defense.loc[defense['year'] >=  1978, ['year', 'defense', 'DER']]
-der = der.pivot(index='year', column='defense', values='DER')
+der = der.pivot(index='year', columns='defense', values='DER')
 der.plot(x_compat=True, xticks=range(1978,2018,4), rot=45)
 plt.show()
